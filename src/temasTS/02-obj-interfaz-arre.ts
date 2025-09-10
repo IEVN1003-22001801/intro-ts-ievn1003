9d3a577244824a86a4f94751174ad636cd72eb00
Litzy Bautista <lbautistalop@gamil.com>
2025-09-09 18:42:18 -0600

    DesestructuracionDeDatos

---
+++ src/temasTS/02-obj-interfaz-arre.ts
@@ -0,0 +1,31 @@
+
+//Al crear una interfaz no tenemos definir el tipo tan precisamente 
+// como asignar ya el nombre, si no que se asigne solo 
+interface Alumno{
+    nombre:string,
+    edad:number,
+    activo:boolean,
+    email?:string, //el ? indica que es opcional
+    nota:number| string //puede ser número o string
+}
+
+const alumno:Alumno={
+    nombre:"Juan",
+    edad:23,
+    activo:true,
+    email:"juan@gmail.com",
+    nota:9.5 //esta "nota" no es opcional si la quitamos nos dara error
+}
+console.table(alumno) //imprimimos para visualizar: npm run dev
+
+let mascotas:string[]=['perro','gato', 'loro']
+mascotas.push('tortuga')
+console.log(mascotas)
+mascotas[1]='pez'
+console.log(mascotas)
+
+let idades:(number|string)[]=[23,45,67,'89']
+idades.push(90)
+idades.push('100')
+console.log(idades)
+
